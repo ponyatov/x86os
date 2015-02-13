816a47c4090d8af122ae182015cebd7df472a297
Dmitry Ponyatov <dponyatov@gmail.com>
2015-02-13 16:34:07 +0400

    +mk/versions
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ all:
 .PHONY: clean
 clean:
 
+include mk/versions.mk
 include mk/dirs.mk
 include mk/src.mk
 include mk/cross.mk
--- mk/src.mk
+++ mk/src.mk
@@ -1,10 +1,4 @@
 
-BINUTILS_VER = 2.24
-GMP_VER	= 5.1.3
-MPFR_VER = 3.1.2
-MPC_VER = 1.0.2
-GCC_VER = 4.9.1
-
 BINUTILS = binutils-$(BINUTILS_VER)
 GMP = gmp-$(GMP_VER)
 MPFR = mpfr-$(MPFR_VER)
