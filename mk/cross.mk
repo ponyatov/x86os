6b83c5b4e94dd8445c23df0920b41df36ea1a43f
Dmitry Ponyatov <dponyatov@gmail.com>
2015-02-13 16:31:37 +0400

    * binutils
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cross.mk
+++ mk/cross.mk
@@ -1,7 +1,17 @@
 
 TARGET = $(CPU)-elf
+
+CFG = configure --disable-nls --disable-werror
+
 .PHONY: cross
 cross: binutils
 
+CFG_BINUTILS = --target=$(TARGET) --prefix=$(TC) \
+	--enable-lto --disable-multilib
+
 .PHONY: binutils
 binutils: $(SRC)/$(BINUTILS)/README
+	rm -rf $(TMP)/$(BINUTILS) && mkdir $(TMP)/$(BINUTILS) &&\
+	cd $(TMP)/$(BINUTILS) &&\
+	$(SRC)/$(BINUTILS)/$(CFG) $(CFG_BINUTILS) &&\
+	make -j2 && make install-strip
